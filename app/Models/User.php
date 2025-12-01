d1c32c665f9b6e7b1bf49fe2d0616cb3871cf9f1
Pedro Lucas <pedrocabral1707@gmail.com>
2025-12-01 13:55:20 -0300

    teste

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,61 +2,34 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use App\Http\Controllers\Admin\Usercontroller;
+use App\Http\Middleware\AdminMiddleware;
 
 class User extends Authenticatable
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var list<string>
-     */
     protected $fillable = [
-        'name',
-        'email',
-        'password',
-        'last_activity', // Adicionado last_activity ao fillable
+        'name', 'email', 'password', 'last_activity', 'is_admin', // <--- Importante
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var list<string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
+    protected $hidden = ['password', 'remember_token'];
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
     protected function casts(): array
     {
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
-            'last_activity' => 'datetime', // Adicionado o cast para datetime
+            'last_activity' => 'datetime',
+            'is_admin' => 'boolean',
         ];
     }
 
-    // Método para verificar se o usuário está online
     public function isOnline()
     {
-        return $this->last_activity && $this->last_activity->diffInMinutes(now()) < 5; // Ajuste o tempo conforme necessário
-    }
-
-    // Método para atualizar a última atividade do usuário
-    public function updateLastActivity()
-    {
-        $this->update(['last_activity' => now()]);
+        return $this->last_activity && $this->last_activity->diffInMinutes(now()) < 5;
     }
 }
